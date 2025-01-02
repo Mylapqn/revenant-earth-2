0575ddc2155f564c422857cd331e4f8cef1ccd55
Mylapqn <mylapqn312@gmail.com>
2025-01-02 16:27:54 +0100

    create demo scene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,57 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Application, Assets, Point, Sprite, Texture } from 'pixi.js'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = new Application();
+const player = new Sprite();
+const robo = new Sprite();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function init() {
+    await app.init({ background: '#1099bb', resizeTo: window });
+    document.body.appendChild(app.canvas);
+
+    const bg = new Sprite(await Assets.load('./bg.png'));
+    bg.scale.set(1);
+    app.stage.addChild(bg);
+
+    player.texture = await Assets.load('./char.png');
+    player.texture.source.scaleMode = 'nearest';
+    player.anchor.set(0.5);
+    player.scale.set(4);
+    player.scale.x *= -1;
+    app.stage.addChild(player);
+
+    robo.texture = await Assets.load('./robo.png');
+    robo.texture.source.scaleMode = 'nearest';
+    robo.anchor.set(0.5);
+    robo.scale.set(4);
+    app.stage.addChild(robo);
+
+    requestAnimationFrame(update);
+}
+
+init();
+
+
+function update() {
+
+    let x = (player.position.x - robo.position.x)*.01+robo.position.x;
+    let y = (player.position.y - robo.position.y)*.01+robo.position.y;
+    
+    robo.scale.x = x < robo.position.x? -4 : 4;
+    robo.position.set(x, y);
+
+
+    player.x = mousePos.x;
+    player.y = mousePos.y;
+
+    app.render();
+    requestAnimationFrame(update);
+}
+
+
+const mousePos = { x: 0, y: 0 };
+
+document.addEventListener('mousemove', (e) => {
+    mousePos.x = e.clientX;
+    mousePos.y = e.clientY;
+});
