e0dc7547564fc7b8287b98de0535b44cf9e9058a
Mylapqn <mylapqn312@gmail.com>
2025-03-13 16:35:48 +0100

    round pixels

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import './style.css'
 
 async function init() {
     const app = new Application();
-    await app.init({ background: '#1099bb', resizeTo: window, antialias: false, powerPreference: "high-performance"});
+    await app.init({ background: '#1099bb', resizeTo: window, antialias: false, powerPreference: "high-performance",roundPixels:false});
     document.body.appendChild(app.canvas);
 
     const game = new Game(app);
